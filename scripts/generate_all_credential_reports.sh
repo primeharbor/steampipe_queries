11fb265b878612151c3f80c6b677ae2f18680a9a
Chris Farris <chris@room17.com>
2022-11-03 09:43:32 -0400

    Use the active AWS_CONFIG_FILE

--- scripts/generate_all_credential_reports.sh
+++ scripts/generate_all_credential_reports.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-PROFILES=`grep profile ~/.aws/config  | awk '{print $2}' | sed s/\]//g`
+if [ -z ${AWS_CONFIG_FILE+x} ] ; then
+	AWS_CONFIG_FILE="~/.aws/config"
+fi
+
+PROFILES=`grep '\[profile' $AWS_CONFIG_FILE  | awk '{print $2}' | sed s/\]//g`
 
 for p in $PROFILES ; do
 	echo "Generating credential report in $p"

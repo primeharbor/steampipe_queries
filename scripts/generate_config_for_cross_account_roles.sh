e25c30467099f42d68575c18239d5da743edd14a
Chris Farris <chris@room17.com>
2022-11-08 11:17:43 -0500

    create file for SSO or Cross-Account-Role

--- scripts/generate_config_for_cross_account_roles.sh
+++ scripts/generate_config_for_cross_account_roles.sh
@@ -1,11 +1,30 @@
 #!/bin/bash
 
-AUDITROLE="$1"
-PAYER_PROFILE="$2"
+COMMAND=$1
+AUDITROLE=$2
+AWS_CONFIG_FILE=$3
+SSO_PROFILE=$4
 
-if [ -z $AUDITROLE ] ; then
-	echo "Usage: $0 <AUDITROLE>"
-	exit 1
+usage () {
+  echo "Usage: $0 [IMDS | SSO ] <AUDITROLE> <AWS_CONFIG_FILE> <SSO_PROFILE>"
+  exit 1
+}
+
+if [ -z "$COMMAND" ] ; then
+  usage
+fi
+
+if [ $COMMAND != "IMDS" ] && [ $COMMAND != "SSO" ] ; then
+  echo "Invalid Command"
+  usage
+fi
+
+if [ $COMMAND == "IMDS" ] && [ -z $AWS_CONFIG_FILE ] ; then
+  usage
+fi
+
+if [ $COMMAND == "SSO" ] && [ -z $SSO_PROFILE ] ; then
+  usage
 fi
 
 # STEAMPIPE_INSTALL_DIR overrides the default steampipe directory of ~/.steampipe
@@ -14,19 +33,13 @@ if [ -z $STEAMPIPE_INSTALL_DIR ] ; then
   export STEAMPIPE_INSTALL_DIR=~/.steampipe
 fi
 
-# the AWS_CONFIG_FILE env variable overrides the default location for the ~/.aws/config file
-if [ -z $AWS_CONFIG_FILE ] ; then
-  echo "AWS_CONFIG_FILE not defined. Setting one"
-  export AWS_CONFIG_FILE=~/.aws/config
-fi
-
 if [ ! -d $STEAMPIPE_INSTALL_DIR ] ; then
   echo "$STEAMPIPE_INSTALL_DIR doesn't exist. Creating it"
   mkdir -p ${STEAMPIPE_INSTALL_DIR}/config/
 fi
 
-if [ ! -f $AWS_CONFIG_FILE ] ; then
-  echo "$AWS_CONFIG_FILE doesn't exist. Aborting"
+if [ -f $AWS_CONFIG_FILE ] ; then
+  echo "$AWS_CONFIG_FILE exists. Aborting rather than overwriting a critical file."
   exit 1
 fi
 
@@ -35,14 +48,15 @@ ALL_REGIONS='["*"]'
 
 echo "Creating Steampipe Connections in $SP_CONFIG_FILE and AWS Profiles in $AWS_CONFIG_FILE"
 echo "# Automatically Generated at `date`" > $SP_CONFIG_FILE
-echo "# Automatically Generated at `date`" > $AWS_CONFIG_FILE
+echo "# Steampipe profiles, Automatically Generated at `date`" > $AWS_CONFIG_FILE
 
+if [ $COMMAND == "IMDS" ] ; then
 # Your AWS Config file needs a [default] section
-echo <<EOF>>$AWS_CONFIG_FILE
+cat <<EOF>>$AWS_CONFIG_FILE
 [default]
 region = us-east-1
-
 EOF
+fi
 
 cat <<EOF>>$SP_CONFIG_FILE
 
@@ -51,13 +65,6 @@ connection "aws" {
   plugin = "aws"
   type        = "aggregator"
   connections = ["aws_*"]
-}
-
-connection "aws_payer" {
-  plugin = "aws"
-  type        = "aggregator"
-  connections = ["$PAYER_PROFILE"]
-  regions = ["us-east-1"] # This aggregator is only used for global queries
 }
 
 EOF
@@ -72,30 +79,40 @@ while read line ; do
   # Steampipe doesn't like dashes, so we need to swap for underscores
   SP_NAME=`echo $ACCOUNT_NAME | sed s/-/_/g`
 
+if [ $COMMAND == "IMDS" ] ; then
 # Append an entry to the AWS Creds file
 cat <<EOF>>$AWS_CONFIG_FILE
 
-[profile $ACCOUNT_NAME]
-role_arn = arn:aws:iam::$ACCOUNT_ID:role/$AUDITROLE
+[profile sp_${ACCOUNT_NAME}]
+role_arn = arn:aws:iam::${ACCOUNT_ID}:role/${AUDITROLE}
 credential_source = Ec2InstanceMetadata
 role_session_name = steampipe
 EOF
+else
+cat <<EOF>>$AWS_CONFIG_FILE
+
+[profile sp_${ACCOUNT_NAME}]
+role_arn = arn:aws:iam::${ACCOUNT_ID}:role/${AUDITROLE}
+source_profile = ${SSO_PROFILE}
+role_session_name = steampipe
+EOF
+fi
 
 # And append an entry to the Steampipe config file
 cat <<EOF>>$SP_CONFIG_FILE
-connection "aws_$SP_NAME" {
+connection "aws_${SP_NAME}" {
   plugin  = "aws"
-  profile = "$ACCOUNT_NAME"
-  regions = $ALL_REGIONS
-    options "connection" {
-        cache     = true # true, false
-        cache_ttl = 3600  # expiration (TTL) in seconds
-    }
+  profile = "sp_${ACCOUNT_NAME}"
+  regions = ${ALL_REGIONS}
 }
 
 EOF
 
 done < <(aws organizations list-accounts --query Accounts[].[Name,Id,Status] --output text)
+
+if [ $COMMAND == "SSO" ] ; then
+  echo "Now append $AWS_CONFIG_FILE to your active config file where $SSO_PROFILE is defined"
+fi
 
 # All done!
 exit 0
--- scripts/generate_config_for_sso.sh
+++ scripts/generate_config_for_sso.sh
@@ -1,39 +1,70 @@
 #!/bin/bash
 
-AUDITROLE=$1
-SSO_PROFILE=$2
-PAYER_PROFILE=$3
-AWS_CONFIG_FILE=$4
+SSO_PREFIX=$1
+SSO_ROLE=$2
+STEAMPIPE_CONFIG=$3
+AWS_CONFIG=$4
 
-if [ -z $AWS_CONFIG_FILE ] ; then
-	echo "Usage: $0 <AUDITROLE> <SSO_PROFILE> <PAYER_PROFILE> <AWS_CONFIG_FILE>"
-	exit 1
-fi
-
-# STEAMPIPE_INSTALL_DIR overrides the default steampipe directory of ~/.steampipe
-if [ -z $STEAMPIPE_INSTALL_DIR ] ; then
-  echo "STEAMPIPE_INSTALL_DIR not defined. Setting one"
-  export STEAMPIPE_INSTALL_DIR=~/.steampipe
-fi
-
-if [ ! -d $STEAMPIPE_INSTALL_DIR ] ; then
-  echo "$STEAMPIPE_INSTALL_DIR doesn't exist. Creating it"
-  mkdir -p ${STEAMPIPE_INSTALL_DIR}/config/
-fi
-
-if [ -f $AWS_CONFIG_FILE ] ; then
-  echo "$AWS_CONFIG_FILE exists. Aborting rather than overwriting a critical file."
+if [ -z "$AWS_CONFIG" ] ; then
+  echo "Missing SSO Prefix"
+  echo "Usage: $0 <SSO_PREFIX> <SSO_ROLE> <STEAMPIPE_CONFIG> <AWS_CONFIG>"
   exit 1
 fi
 
-SP_CONFIG_FILE=${STEAMPIPE_INSTALL_DIR}/config/aws.spc
-ALL_REGIONS='["*"]'
+START_URL="https://${SSO_PREFIX}.awsapps.com/start"
 
-echo "Creating Steampipe Connections in $SP_CONFIG_FILE and AWS Profiles in $AWS_CONFIG_FILE"
-echo "# Automatically Generated at `date`" > $SP_CONFIG_FILE
-echo "# Steampipe profiles, Automatically Generated at `date`" > $AWS_CONFIG_FILE
+aws sso-oidc register-client --client-name 'profiletool' --client-type 'public' --region "${AWS_DEFAULT_REGION}" > client.json
 
-cat <<EOF>>$SP_CONFIG_FILE
+# Returns:
+# {
+#     "clientId": "REDACTED",
+#     "clientSecret": "REDACTED,
+#     "clientIdIssuedAt": 1667594405,
+#     "clientSecretExpiresAt": 1675370405
+# }
+
+clientid=`cat client.json | jq .clientId -r`
+secret=`cat client.json | jq .clientSecret -r`
+rm client.json
+
+aws sso-oidc start-device-authorization --client-id "$clientid" --client-secret "$secret" --start-url "${START_URL}" --region "${AWS_DEFAULT_REGION}" > device_auth.json
+
+# Returns:
+# {
+#     "deviceCode": "REDACTED",
+#     "userCode": "RHHX-BCTS",
+#     "verificationUri": "https://device.sso.us-east-1.amazonaws.com/",
+#     "verificationUriComplete": "https://device.sso.us-east-1.amazonaws.com/?user_code=RHHX-BCTS",
+#     "expiresIn": 600,
+#     "interval": 1
+# }
+
+auth_url=`cat device_auth.json | jq -r .verificationUriComplete`
+devicecode=`cat device_auth.json | jq -r .deviceCode`
+rm device_auth.json
+
+open $auth_url
+
+echo "$auth_url was opened in your browser. Please click allow."
+echo "Press Enter when complete"
+read s
+
+token=`aws sso-oidc create-token --client-id "$clientid" --client-secret "$secret" --grant-type 'urn:ietf:params:oauth:grant-type:device_code' --device-code "$devicecode" --region "${AWS_DEFAULT_REGION}" --query accessToken --output text`
+
+# Returns:
+# {
+#     "accessToken": "REDACTED",
+#     "tokenType": "Bearer",
+#     "expiresIn": 14839
+# }
+
+
+echo "Creating Steampipe Connections in $STEAMPIPE_CONFIG and AWS Profiles in $AWS_CONFIG"
+echo "# Automatically Generated at `date`" > $STEAMPIPE_CONFIG
+echo "# Steampipe profiles, Automatically Generated at `date`" > $AWS_CONFIG
+
+
+cat <<EOF>>$STEAMPIPE_CONFIG
 
 # Create an aggregator of _all_ the accounts as the first entry in the search path.
 connection "aws" {
@@ -42,50 +73,42 @@ connection "aws" {
   connections = ["aws_*"]
 }
 
-# create an aggregator of just the payer
-connection "aws_payer" {
-  plugin = "aws"
-  type        = "aggregator"
-  connections = ["$PAYER_PROFILE"]
-  regions = ["us-east-1"] # This aggregator is only used for global queries
-}
-
 EOF
 
-# We now iterate across the `aws organizations list-accounts` command
-while read line ; do
 
-  # extract the values we need
-  ACCOUNT_NAME=`echo $line | awk '{print $1}'`
-  ACCOUNT_ID=`echo $line | awk '{print $2}'`
+for a in `aws sso list-accounts --access-token "$token" --region "${AWS_DEFAULT_REGION}" --output text | awk '{print $2":"$3}'` ; do
+
+  acctnum=`echo $a | awk -F: '{print $1}'`
+  acctname=`echo $a | awk -F: '{print $2}'`
 
   # Steampipe doesn't like dashes, so we need to swap for underscores
-  SP_NAME=`echo $ACCOUNT_NAME | sed s/-/_/g`
+  SP_NAME=`echo $acctname | sed s/-/_/g`
 
-# Append an entry to the AWS Creds file
-cat <<EOF>>$AWS_CONFIG_FILE
+  aws sso list-account-roles --account-id "$acctnum" --access-token "$token" --region "${AWS_DEFAULT_REGION}" | grep $SSO_ROLE > /dev/null
+  if [ $? -ne 0 ] ; then
+    echo "# $SSO_ROLE was not in list of SSO roles available to this user. Skipping account $acctname ($acctnum) \n"
+  else
 
-[profile sp_${ACCOUNT_NAME}]
-role_arn = arn:aws:iam::${ACCOUNT_ID}:role/${AUDITROLE}
-source_profile = ${SSO_PROFILE}
-role_session_name = steampipe-sso
+cat << EOF>> AWS_CONFIG
+
+[profile ${acctname}]
+sso_start_url = ${START_URL}
+sso_region = ${AWS_DEFAULT_REGION}
+sso_account_id = ${acctnum}
+sso_role_name = ${SSO_ROLE}
 EOF
+  fi
 
 # And append an entry to the Steampipe config file
-cat <<EOF>>$SP_CONFIG_FILE
+cat <<EOF>>$STEAMPIPE_CONFIG
 connection "aws_${SP_NAME}" {
   plugin  = "aws"
-  profile = "sp_${ACCOUNT_NAME}"
-  regions = ${ALL_REGIONS}
-    options "connection" {
-        cache     = true # true, false
-        cache_ttl = 3600  # expiration (TTL) in seconds
-    }
+  profile = "${acctname}"
+  regions = ["*"]
 }
 
 EOF
 
-done < <(aws organizations list-accounts --query Accounts[].[Name,Id,Status] --output text)
 
-# All done!
-exit 0
+done
+

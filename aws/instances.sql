752270c700d40e3ee905bbc7f7f41bd972e4d16b
Chris Farris <chris.farris@playonsports.com>
2023-05-17 11:22:43 -0400

    Remove duplicate lines that comes form the SG Name

--- aws/instances.sql
+++ aws/instances.sql
@@ -3,7 +3,6 @@ SELECT ec2.instance_id, ec2.instance_type, ec2.instance_state, ec2.image_id, ec2
 	ec2.private_ip_address,
 	ec2.public_ip_address,
 	ec2.tags ->> 'Name' as instance_name,
-	jsonb_array_elements(ec2.security_groups) ->> 'GroupName' as security_group_name,
 	org.name as account_name,
 	org.id as account_id
 FROM aws_ec2_instance as ec2,

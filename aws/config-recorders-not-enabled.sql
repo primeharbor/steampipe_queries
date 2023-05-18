bfa363eab7b1408521da60f5d3c3a2df5a80d8a7
Chris Farris <chris.farris@playonsports.com>
2023-05-18 08:35:36 -0400

    Update config-recorders-not-enabled.sql

--- aws/config-recorders-not-enabled.sql
+++ aws/config-recorders-not-enabled.sql
@@ -2,7 +2,9 @@
 -- This query returns a list of all account/regions where config is not enabled
 -- Remove AND r.name IS NULL to see a full report
 
-SELECT r.name, a.region, r.status_recording, a.account_id
+SELECT r.name, a.region, r.status_recording, a.account_id, 
+	r.recording_group ->> 'AllSupported' as AllSupported,
+	r.recording_group ->> 'IncludeGlobalResourceTypes' as GlobalTypes
 FROM aws_region AS a
 FULL OUTER JOIN aws_config_configuration_recorder AS r 
 	ON (r.account_id = a.account_id AND a.region = r.region)

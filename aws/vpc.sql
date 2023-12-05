50d6c1f401eee5fb33eaf3f2ae7e86ec89f35fb1
Chris Farris <chris@room17.com>
2023-12-05 13:32:55 -0500

    User CTX name over joining to the org

--- aws/vpc.sql
+++ aws/vpc.sql
@@ -2,12 +2,9 @@ SELECT
 	vpc.vpc_id AS vpc_id,
 	vpc.tags ->> 'Name' as VPC_Name,
 	vpc.account_id AS account_id,
-	org.name AS account_name,
 	vpc.cidr_block AS vpc_cidr_block,
 	vpc.is_default AS default,
-	vpc.region AS region
+	vpc.region AS region,
+	vpc._ctx ->> 'connection_name' AS account_name
 FROM
-	aws_vpc AS vpc,
-	aws_payer.aws_organizations_account AS org
-WHERE
-	org.id = vpc.account_id
+	aws_vpc AS vpc;
